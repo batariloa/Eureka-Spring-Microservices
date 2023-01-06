e2a3ad9cc6f16c11c007d938c3825b973d2bc7ef
batariloa <batariloa@icloud.com>
2023-01-06 18:45:08 +0100

    Eureka

--- order-service/src/main/java/com/batarilo/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/batarilo/orderservice/OrderServiceApplication.java
@@ -1,10 +1,17 @@
 package com.batarilo.orderservice;
 
+import com.netflix.discovery.EurekaClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class OrderServiceApplication {
+
+    @Lazy
+    private EurekaClient eurekaClient;
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
--- product-service/src/main/java/com/batariloa/product_service_testcontainer/ProductServiceTestcontainerApplication.java
+++ product-service/src/main/java/com/batariloa/product_service_testcontainer/ProductServiceTestcontainerApplication.java
@@ -1,10 +1,20 @@
 package com.batariloa.product_service_testcontainer;
 
+import com.netflix.discovery.EurekaClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
-public class ProductServiceTestcontainerApplication {
+@RequiredArgsConstructor
+
+public class ProductServiceTestcontainerApplication  {
+
+
+    @Lazy
+    private EurekaClient eurekaClient;
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceTestcontainerApplication.class, args);
